d5337c9ea2444252eca5d4e2c38ad26f4e0e4d23
Kalyan-Nathari <64293871+Kalyan-Nathari@users.noreply.github.com>
2021-04-08 15:13:45 +0530

    added 3 numbers

--- Exercise-2-Add numbers/index.js
+++ Exercise-2-Add numbers/index.js
@@ -1,8 +1,8 @@
 function addNumbers() {
   var num1 = document.getElementById("num1");
   var num2 = document.getElementById("num2");
-
-  var sum = Number(num1.value) + Number(num2.value);
+  var num3 = document.getElementById("num3");
+  var sum = Number(num1.value) + Number(num2.value) +Number(num3.value);
 
   var result = document.getElementById("result");
   result.innerHTML = sum;
